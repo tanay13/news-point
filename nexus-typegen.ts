ea66c543427a205c40cc95c4ba56c81cf8d5f053
Tanay Raj <tanay.raj76@gmail.com>
2022-01-29 13:43:11 +0530

    filtering add

--- nexus-typegen.ts
+++ nexus-typegen.ts
@@ -166,6 +166,11 @@ export interface NexusGenArgTypes {
       linkId: number; // Int!
     }
   }
+  Query: {
+    feed: { // args
+      filter?: string | null; // String
+    }
+  }
 }
 
 export interface NexusGenAbstractTypeMembers {
